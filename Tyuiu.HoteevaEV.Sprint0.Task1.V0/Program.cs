512d7866081466357ee546b548a5ddc1f3121ec2
Tyuiu.HoteevaEV@outlook.com <172helena@gmail.com>
2024-09-21 14:02:50 +0500

    Task 1

---
+++ Tyuiu.HoteevaEV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.HoteevaEV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Елена";
+            int age = 18;
+            Console.WriteLine("Здравствуйте! Меня зовут " + name + ". Мне " + age + " лет.");
+            Console.ReadKey();
+        }
+    }
+}
